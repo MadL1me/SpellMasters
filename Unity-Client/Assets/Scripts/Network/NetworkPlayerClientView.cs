5d5b532a910f0cb8083c26d8b839da3d1d6d7894
XmasApple <y4blokof@yandex.ru>
2021-07-13 23:59:31 +0300

    Display name, stamina and health bars

--- Unity-Client/Assets/Scripts/Network/NetworkPlayerClientView.cs
+++ Unity-Client/Assets/Scripts/Network/NetworkPlayerClientView.cs
@@ -1,34 +1,45 @@
-﻿using DG.Tweening;
+﻿using Core.Player;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 namespace MagicCardGame.Network
 {
     public class NetworkPlayerClientView : MonoBehaviour
-    {  
+    {
         public NetworkPlayerClient NetworkPlayer { get; protected set; }
+        public NetworkPlayerStats PlayerStats { get; protected set; }
         [SerializeField] private bool _isLocal;
-        
+        [SerializeField] private TMP_Text _displayNameText;
+        [SerializeField] private RectTransform _healthBar;
+        [SerializeField] private RectTransform _staminaBar;
+
         private void Awake()
         {
             NetworkPlayer = new NetworkPlayerClient(this, _isLocal);
+            PlayerStats = new NetworkPlayerStats();
+            _displayNameText.text = PlayerStats.DisplayName;
         }
 
         public void Update()
         {
             NetworkPlayer?.Update();
+            var healthBarRect = _healthBar.rect;
+            var staminaBarRect = _staminaBar.rect;
+            healthBarRect.width = 100 / PlayerStats.Health;
+            staminaBarRect.width = 100 / PlayerStats.Stamina.Available;
+
         }
 
         public void InitPlayerFromNetwork()
         {
-            
         }
 
         public void Move(Vector2 vector)
         {
             transform.DOMove(new Vector2(
-                transform.position.x + vector.x, 
-                transform.position.y + vector.y),0.5f);
+                transform.position.x + vector.x,
+                transform.position.y + vector.y), 0.5f);
         }
-        
     }
 }
